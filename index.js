416541ab2b9710113b7167a76576630a286a10ca
Dayvid Santana <santana.dayvid@outlook.com>
2022-05-16 14:15:31 -0300

    add connection on Socket

--- index.js
+++ index.js
@@ -9,46 +9,45 @@ app. listen(PORT, () => {
     console.log('Rodando na porta ' + PORT)
 })
 function resposta (req, res) {
-
-    let arquivo = ''
-    if (req.url == '/'){
-        arquivo = __dirname + '/index.html'
+    var arquivo = "";
+    if(req.url == "/"){
+        arquivo = __dirname + '/index.html';
+    }else{
+        arquivo = __dirname + req.url;
     }
-    else{
-        arquivo = __dirname + req.url
-    }
+    fs.readFile(arquivo,
+        function (err, data) {
+             if (err) {
+                  res.writeHead(404);
+                  return res.end('Vish! deu ruim')
+             }
 
-    fs.readFile(__dirname + '/index.html', (err, data) => {
-        if (err) {
-            res.writeHead(404)
-            return res.end('A página ou o arquivo não foi encontrado')
+             res.writeHead(200);
+             res.end(data);
         }
-
-        res.writeHead(200)
-        res.end(data)
-        console.info('O servidor está rodando corretamente.')
-    })
-    
+    );
 }
 
-io.on('connection', (socket) => {
-    socket.on('enviar mensagem', function(mensagem_enviada, callback){
-        mensagem_enviada = '[ ' + pegarDataAtual() + ' ]:' + mensagem_enviada
+io.on("connection", function(socket){
+    socket.on("enviar mensagem", function(mensagem_enviada, callback){
+        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + mensagem_enviada;
 
-        io.sockets.emit('atualizar mensagens', mensagem_enviada)
-        callback()
-    })
-})
+        io.sockets.emit("atualizar mensagens", mensagem_enviada);
+        callback();
+    });
+});
 
 function pegarDataAtual(){
-    var dataAtual = new Date();
-    var dia = (dataAtual.getDate()<10 ? '0' : '') + dataAtual.getDate();
-    var mes = ((dataAtual.getMonth() + 1)<10 ? '0' : '') + (dataAtual.getMonth() + 1);
-    var ano = dataAtual.getFullYear();
-    var hora = (dataAtual.getHours()<10 ? '0' : '') + dataAtual.getHours();
-    var minuto = (dataAtual.getMinutes()<10 ? '0' : '') + dataAtual.getMinutes();
-    var segundo = (dataAtual.getSeconds()<10 ? '0' : '') + dataAtual.getSeconds();
+    let dataAtual = new Date();
+    let dia = (dataAtual.getDate()<10 ? '0' : '') + dataAtual.getDate();
+    let mes = ((dataAtual.getMonth() + 1)<10 ? '0' : '') + (dataAtual.getMonth() + 1);
+    let ano = dataAtual.getFullYear();
+    let hora = (dataAtual.getHours()<10 ? '0' : '') + dataAtual.getHours();
+    let minuto = (dataAtual.getMinutes()<10 ? '0' : '') + dataAtual.getMinutes();
+    let segundo = (dataAtual.getSeconds()<10 ? '0' : '') + dataAtual.getSeconds();
   
-    var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
+    let dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
     return dataFormatada;
 }
+
+
