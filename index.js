9f9a398f147790505b3b8c3def63123d0842c32e
Dayvid Santana <santana.dayvid@outlook.com>
2022-05-16 15:20:31 -0300

    add user on chat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('http').createServer(resposta)
 const fs = require('fs')
 const io = require('socket.io')(app);
-
+const usuario = []
 
 
 const PORT = 8080
@@ -29,13 +29,28 @@ function resposta (req, res) {
 }
 
 io.on("connection", function(socket){
+
+    // Verificação de usuários
+    socket.on("entrar", function(apelido, callback){
+        if(!(apelido in usuarios)){
+            socket.apelido = apelido;
+            usuarios[apelido] = socket;
+            callback(true);
+        }else{
+            callback(false);
+        }
+    });
+
     socket.on("enviar mensagem", function(mensagem_enviada, callback){
-        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + mensagem_enviada;
+        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + socket.apelido + ": " + mensagem_enviada;
 
         io.sockets.emit("atualizar mensagens", mensagem_enviada);
         callback();
     });
 });
+
+
+
 
 function pegarDataAtual(){
     let dataAtual = new Date();
@@ -51,3 +66,25 @@ function pegarDataAtual(){
 }
 
 
+// talvez tenha que por isso no script do html
+
+// Login para o chat
+$("form#login").submit(function(e){
+
+    e.preventDefault();
+
+    socket.emit("entrar", 
+    $(this).find("#apelido").val(), 
+    function(valido){
+
+        if(valido){
+             $("#acesso_usuario").hide();
+             $("#sala_chat").show();
+        }else{
+             $("#acesso_usuario").val("");
+             alert("Nome já utilizado nesta sala");
+        }
+        
+    });
+
+});
