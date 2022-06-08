73f3c5c33a09d9c07575f3cc19ef286eaa146efd
Dayvid Santana <santana.dayvid@outlook.com>
2022-06-08 09:34:08 -0300

    add conversation bot

---
+++ call/main.py
@@ -0,0 +1,1 @@
+# Função para ligar para o morador
--- conversationBOT.js
+++ conversationBOT.js
@@ -2,6 +2,23 @@ const chamar = document.querySelector('#chamar-morador')
 const sala_chat = document.querySelector('#sala_chat')
 const acessoUsuario = sala_chat.querySelector('#chat_bot')
 
-chamar.addEventListener('click', () => {
-    acessoUsuario.classList.toggle('off')
-})
+//let mensagemEspera = `OK, ${visitante}! Vou chamar ${moradorSolicitado} agora mesmo. Por favor, espere um momento!`
+
+
+// Pessoas
+const nomeVisitante = document.querySelector('#visitante').value
+const moradorSolicitado = document.querySelector('#moradorSolicitado').value
+
+if (nomeVisitante === undefined) {
+    alert('Digite o nome do visitante!')
+}
+else {
+    chamar.addEventListener('click', () => {
+        acessoUsuario.classList.toggle('off')
+        document.querySelector('#respostaBotSaudacao').innerHTML = `Por favor, espere um ${nomeVisitante} momento! `
+        //document.querySelector('#respostaBotSaudacao').innerHTML = `OK, ${visitante}! Vou chamar ${moradorSolicitado} agora mesmo. Por favor, espere um momento!`
+    })
+}
+
+// função que ativa função no python para ligar para morador
+
